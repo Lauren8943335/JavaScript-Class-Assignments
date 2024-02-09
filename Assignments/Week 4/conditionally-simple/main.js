c0563b82a2de354c29518beeadccaf73e51e68a5
Lauren8943335 <lrugless.prsvr@gmail.com>
2024-02-08 21:12:13 -0600

    work

--- Assignments/Week 4/conditionally-simple/main.js
+++ Assignments/Week 4/conditionally-simple/main.js
@@ -2,8 +2,50 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(){ 
+  let 
+}
+var isItLong = ("That's a long string!"); 
+console.log(isItLong.length)
+if (isItLong >= 20) { 
+  console.log(isItLong)
+} else { 
+  console.log("undefined")
+}
 
+var isItMedium= ("That's a regular sized string!"); 
+console.log(isItMedium.length)
+if (isItMedium <=10  ) { 
+ console.log(isItMedium)
+}
 
+if (isItShort < 10 ) { 
+console.log("That's a small string!");
+} else if (isItShort >10 ) { 
+console.log("That's not a small string!"); 
+} else if  (isItShort = 10) { 
+   console.log("That's not a small string!"); 
+}
+
+if (howLongIsMyString >= 20) {
+console.log("That's a long string!"); 
+} else if (howLongIsMyString <=20) { 
+ console.log("That's a regular sized string!"); 
+} else if (howLongIsMyString = 10-20) { 
+  console.log("That's a regular sized string!"); 
+} else if (howLongIsMyString=10){
+  console.log("That's a regular sized string!"); 
+ } else { 
+   console.log("That's a regular sized string!"); 
+ }
+
+if (personsHeight = "Shaq") { 
+  console.log ("85"); 
+} else if (personsHeight = "Zooey") {  
+  console.log ("65"); 
+}else { 
+  console.log ("I don't know everyone's height!"); 
+}
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
